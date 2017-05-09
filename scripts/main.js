e6ab69ac6d90211bbe2139a56ccc5000e16da61c
spacegoing <spacegoing@gmail.com>
2017-05-09 20:04:56 +0800

    Finished StarterKit with image file not found errors

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+let myImg = document.querySelector('img');
+
+myImg.onclick = function () {
+    let mySrc = myImg.getAttribute('src');
+    if (mySrc === 'images/firefox-icon.png') {
+        myImage.setAttribute('src', 'images/firefox2.png');
+    } else {
+        myImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+}
+
+
+
+
+
+
+
+
+
